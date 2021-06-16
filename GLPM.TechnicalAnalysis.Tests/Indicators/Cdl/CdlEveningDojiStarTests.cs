639c39573bcdd3a00197dedd0dbc0cb611dcbc3f
phmatray <philippe@atypical.consulting>
2021-06-16 23:49:03 +0200

    write some tests for candles

---
+++ GLPM.TechnicalAnalysis.Tests/Indicators/Cdl/CdlEveningDojiStarTests.cs
@@ -0,0 +1,62 @@
+using System.Linq;
+using AutoFixture;
+using FluentAssertions;
+using Xunit;
+
+namespace GLPM.TechnicalAnalysis.Tests.Indicators.Cdl
+{
+    public class CdlEveningDojiStarTests
+    {
+        [Fact]
+        public void CdlEveningDojiStarDouble()
+        {
+            // Arrange
+            Fixture fixture = new();
+            const int StartIdx = 0;
+            const int EndIdx = 99;
+            double[] open = fixture.CreateMany<double>(count: 100).ToArray();
+            double[] high = fixture.CreateMany<double>(count: 100).ToArray();
+            double[] low = fixture.CreateMany<double>(count: 100).ToArray();
+            double[] close = fixture.CreateMany<double>(count: 100).ToArray();
+            
+            // Act
+            var actualResult = TAMath.CdlEveningDojiStar(
+                StartIdx,
+                EndIdx,
+                open,
+                high,
+                low,
+                close);
+
+            // Assert
+            actualResult.Should().NotBeNull();
+            actualResult.RetCode.Should().Be(RetCode.Success);
+        }
+        
+        [Fact]
+        public void CdlEveningDojiStarFloat()
+        {
+            // Arrange
+            Fixture fixture = new();
+            const int StartIdx = 0;
+            const int EndIdx = 99;
+            float[] open = fixture.CreateMany<float>(count: 100).ToArray();
+            float[] high = fixture.CreateMany<float>(count: 100).ToArray();
+            float[] low = fixture.CreateMany<float>(count: 100).ToArray();
+            float[] close = fixture.CreateMany<float>(count: 100).ToArray();
+            
+            // Act
+            var actualResult = TAMath.CdlEveningDojiStar(
+                StartIdx,
+                EndIdx,
+                open,
+                high,
+                low,
+                close);
+
+            // Assert
+            actualResult.Should().NotBeNull();
+            actualResult.RetCode.Should().Be(RetCode.Success);
+        }
+    }
+}
---
+++ GLPM.TechnicalAnalysis.Tests/Indicators/Cdl/CdlHikkakeTests.cs
@@ -0,0 +1,62 @@
+using System.Linq;
+using AutoFixture;
+using FluentAssertions;
+using Xunit;
+
+namespace GLPM.TechnicalAnalysis.Tests.Indicators.Cdl
+{
+    public class CdlHikkakeTests
+    {
+        [Fact]
+        public void CdlHikkakeDouble()
+        {
+            // Arrange
+            Fixture fixture = new();
+            const int StartIdx = 0;
+            const int EndIdx = 99;
+            double[] open = fixture.CreateMany<double>(count: 100).ToArray();
+            double[] high = fixture.CreateMany<double>(count: 100).ToArray();
+            double[] low = fixture.CreateMany<double>(count: 100).ToArray();
+            double[] close = fixture.CreateMany<double>(count: 100).ToArray();
+            
+            // Act
+            var actualResult = TAMath.CdlHikkake(
+                StartIdx,
+                EndIdx,
+                open,
+                high,
+                low,
+                close);
+
+            // Assert
+            actualResult.Should().NotBeNull();
+            actualResult.RetCode.Should().Be(RetCode.Success);
+        }
+        
+        [Fact]
+        public void CdlHikkakeFloat()
+        {
+            // Arrange
+            Fixture fixture = new();
+            const int StartIdx = 0;
+            const int EndIdx = 99;
+            float[] open = fixture.CreateMany<float>(count: 100).ToArray();
+            float[] high = fixture.CreateMany<float>(count: 100).ToArray();
+            float[] low = fixture.CreateMany<float>(count: 100).ToArray();
+            float[] close = fixture.CreateMany<float>(count: 100).ToArray();
+            
+            // Act
+            var actualResult = TAMath.CdlHikkake(
+                StartIdx,
+                EndIdx,
+                open,
+                high,
+                low,
+                close);
+
+            // Assert
+            actualResult.Should().NotBeNull();
+            actualResult.RetCode.Should().Be(RetCode.Success);
+        }
+    }
+}
