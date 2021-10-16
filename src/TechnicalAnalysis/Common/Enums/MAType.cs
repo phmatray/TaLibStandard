239a3f205932f007fccee60f68ef535cce8cad99
Philippe Matray <philippe.matray@computerland.be>
2021-10-16 15:47:04 +0200

    Document MAType

--- src/TechnicalAnalysis/Common/Enums/MAType.cs
+++ src/TechnicalAnalysis/Common/Enums/MAType.cs
@@ -1,15 +1,53 @@
 ﻿namespace TechnicalAnalysis.Common
 {
+    /// <summary>
+    /// Moving Average Type
+    /// </summary>
     public enum MAType
     {
+        /// <summary>
+        /// Simple Moving Average
+        /// </summary>
         Sma,
+
+        /// <summary>
+        /// Exponential Moving Average
+        /// </summary>
         Ema,
+        
+        /// <summary>
+        /// Weight Moving Average
+        /// </summary>
         Wma,
+        
+        /// <summary>
+        /// Double Exponential Moving Average
+        /// </summary>
         Dema,
+        
+        /// <summary>
+        /// Triple Exponential Moving Average
+        /// </summary>
         Tema,
+        
+        /// <summary>
+        /// Triangular Moving Average
+        /// </summary>
         Trima,
+        
+        /// <summary>
+        /// Kaufman's Adaptive Moving Average
+        /// </summary>
         Kama,
+        
+        /// <summary>
+        /// MESA Adaptive Moving Average
+        /// </summary>
         Mama,
+        
+        /// <summary>
+        /// Tilson T3 Moving Average
+        /// </summary>
         T3
     }
 }
