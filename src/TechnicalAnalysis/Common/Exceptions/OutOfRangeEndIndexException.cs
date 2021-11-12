bebc2a7412fc4347f11225c9c036030129010915
Philippe Matray <phmatray@gmail.com>
2021-11-12 12:10:57 +0100

    make the exceptions serializable

--- src/TechnicalAnalysis/Common/Exceptions/OutOfRangeEndIndexException.cs
+++ src/TechnicalAnalysis/Common/Exceptions/OutOfRangeEndIndexException.cs
@@ -1,9 +1,24 @@
+using System.Runtime.Serialization;
+
 namespace TechnicalAnalysis.Common;
 
+/// <summary>
+/// Out of range end index exception
+/// </summary>
+[Serializable]
 public class OutOfRangeEndIndexException : Exception
 {
-    public OutOfRangeEndIndexException()
+    /// <summary>
+    /// Constructor for <see cref="OutOfRangeEndIndexException"/> class.
+    /// </summary>
+    protected OutOfRangeEndIndexException()
         : base("End index is out of range")
+    {
+    }
+    
+    /// <inheritdoc cref="Exception"/>
+    protected OutOfRangeEndIndexException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
     {
     }
 }
--- src/TechnicalAnalysis/Common/Exceptions/OutOfRangeStartIndexException.cs
+++ src/TechnicalAnalysis/Common/Exceptions/OutOfRangeStartIndexException.cs
@@ -1,9 +1,24 @@
+using System.Runtime.Serialization;
+
 namespace TechnicalAnalysis.Common;
 
+/// <summary>
+/// Out of range start index exception
+/// </summary>
+[Serializable]
 public class OutOfRangeStartIndexException : Exception
 {
-    public OutOfRangeStartIndexException()
+    /// <summary>
+    /// Constructor for <see cref="OutOfRangeStartIndexException"/> class.
+    /// </summary>
+    protected OutOfRangeStartIndexException()
         : base("Start index is out of range")
+    {
+    }
+
+    /// <inheritdoc cref="Exception"/>
+    protected OutOfRangeStartIndexException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
     {
     }
 }
