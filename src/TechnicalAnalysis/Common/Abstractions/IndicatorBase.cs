dd489b3a36418eed59f46b9a8637bc0e9bd4bcc9
Philippe Matray <phmatray@gmail.com>
2023-03-27 22:58:49 +0200

    Comments IndicatorBase

--- src/TechnicalAnalysis/Common/Abstractions/IndicatorBase.cs
+++ src/TechnicalAnalysis/Common/Abstractions/IndicatorBase.cs
@@ -1,7 +1,16 @@
 ﻿namespace TechnicalAnalysis.Common
 {
+    /// <summary>
+    /// Represents an abstract base class for technical indicators.
+    /// </summary>
     public abstract record IndicatorBase
     {
+        /// <summary>
+        /// Initializes a new instance of the IndicatorBase class.
+        /// </summary>
+        /// <param name="retCode">The return code indicating the status of the indicator calculation.</param>
+        /// <param name="begIdx">The beginning index of the calculated output series.</param>
+        /// <param name="nbElement">The number of elements in the calculated output series.</param>
         protected IndicatorBase(RetCode retCode, int begIdx, int nbElement)
         {
             RetCode = retCode;
@@ -9,10 +18,19 @@
             NBElement = nbElement;
         }
 
+        /// <summary>
+        /// Gets the return code indicating the status of the indicator calculation.
+        /// </summary>
         public RetCode RetCode { get; }
 
+        /// <summary>
+        /// Gets the beginning index of the calculated output series.
+        /// </summary>
         public int BegIdx { get; }
 
+        /// <summary>
+        /// Gets the number of elements in the calculated output series.
+        /// </summary>
         public int NBElement { get; }
     }
 }
